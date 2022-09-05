c7876173309dd69c1445e1cdac291ea519acd94a
David Proto <112580660+Dpro03@users.noreply.github.com>
2022-09-05 12:30:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+var topics = "HTML";
+*
+if (topics === 'HTML') {
+  console.logvar shapes = ["triangle", "square", "pentagon", "circle"];
+function listTopics() {if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (randomtopic=== 'CSS') {
+  console.log("Let's study CSS!");
+} else if (randomtopic=== 'Git') {
+  console.log("Let's study Git!");
+} else if (randomtopic=== 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+}
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
+  for(var x = 0; x < shapes.length; x++) {
+   console.log(shapes[x]);
+  
+} else if (topics === 'CSS') {
+  console.logvar shapes = ["triangle", "square", "pentagon", "circle"];
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
+  for(var x = 0; x < shapes.length; x++) {
+   console.log(shapes[x]);
+  
+} else if (topic === 'Git') {
+  console.logvar shapes = ["triangle", "square", "pentagon", "circle"];
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
+  for(var x = 0; x < shapes.length; x++) {
+   console.log(shapes[x]);
+  
+} else if (topics=== 'JavaScript') {
+  console.logvar shapes = ["triangle", "square", "pentagon", "circle"];
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
+  for(var x = 0; x < shapes.length; x++) {
+   console.log(shapes[x]);
+  
+} else {
+  console.log('Please try again!');
+}
+*
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+listTopics()
+console.log('Which topic should we study first?');
+selectTopics()
